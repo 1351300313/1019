62245e9a7b71a3f2bfee457652cac408f56745e1
stu <stu@qq.com>
2016-10-19 11:18:13 +0800

    添加项目文件。

---
+++ WebAppl1/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(WebAppl1.Startup))]
+namespace WebAppl1
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
